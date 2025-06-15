b35cc6a9ad117349956c8f0338f36f132b31d8e0
Riliumph <riliumph@outlook.jp>
2025-06-16 04:48:21 +0900

    [update] completionファイルをdocker imageに含めるように修正

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -7,9 +7,3 @@ if type docker &>/dev/null; then
   docker completion bash >${bash_completion_d}/docker
   echo "source ${bash_completion_d}/docker" >>$HOME/.bashrc
 fi
-
-if type pip &>/dev/null; then
-  pip completion --bash >${bash_completion_d}/pip
-  echo "source ${bash_completion_d}//pip" >>$HOME/.bashrc
-fi
-
