b393eba4bc36039bf7daba3efc9617905237cbdc
Riliumph <riliumph@outlook.jp>
2025-06-28 21:29:18 +0000

    [update] サンプル実行用main更新

--- mathpy/src/main.py
+++ mathpy/src/main.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 from mathpy.pose import pose2d as p2d
+from mathpy.pose import pose3d as p3d
 
-p = np.array([1.0, 1.0, 1.0])
+p = np.array([40.0, 159.104004, 0.0])
 o = np.array([1.0, 1.0, 1.0, 1.0])
-affine = np.array([[1, 0, 2], [0, 1, 3], [0, 0, 1]])
+affine = np.array([[4, 0, 0, 2], [0, -4, 0, 3], [0, 0, 1, 0], [0, 0, 0, 1]])
 answer = np.array([3.0, 4.0, 0.0])
 
-p = p2d.Pose2d(position=p, orientation=o)
-p.apply_affine(affine3x3=affine)
+p = p3d.Pose3d(position=p, orientation=o)
+p.apply_affine(affine4x4=affine)
 
 print(p)
